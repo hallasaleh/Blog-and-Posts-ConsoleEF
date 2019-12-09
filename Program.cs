3b6bdeae5f0d763b2ab4ace3219dd246b846ef1e
Hala Saleh <hsaleh@my.wctc.edu>
2019-12-09 09:24:58 -0600

    Initial
    Creat Posts

---
+++ Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using BlogsConsole.Models;
+using NLog;
+
+namespace BlogsConsole
+{
+    class MainClass
+    {
+        private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+        public static void Main(string[] args)
+        {
+            logger.Info("Program started");
+            try
+            {
+                string choice;
+                do
+                {
+                    Console.WriteLine("1) Display all blogs");
+                    Console.WriteLine("2) Add blog");
+                    Console.WriteLine("3) Create posts");
+                    Console.WriteLine("\"q\" to quit");
+                    choice = Console.ReadLine();
+                    Console.Clear();
+                    logger.Info($"Option {choice} selected");
+                    if (choice == "1")
+                    {
+                        var db = new BloggingContext();
+                        // Display all Blogs from the database
+                        var query = db.Blogs.OrderBy(b => b.Name);
+
+                        Console.WriteLine("All blogs in the database:");
+                        foreach (var item in query)
+                        {
+                            Console.WriteLine(item.Name);
+                        }
+                    }
+                    else if (choice == "2")
+                    {
+                        // ADD Blog
+                        Console.Write("Enter a name for a new Blog: ");
+                        var name = Console.ReadLine();
+
+                        var blog = new Blog { Name = name };
+
+                        var db = new BloggingContext();
+                        db.AddBlog(blog);
+                        logger.Info("Blog added - {name}", name);
+                    }
+                    else if (choice == "3")
+                    {
+
+                        // Create Post
+                        var db = new BloggingContext();
+                        var query = db.Blogs.OrderBy(b => b.BlogId);
+
+                        Console.WriteLine("Select the blog you would to post to:");
+                        foreach (var item in query)
+                        {
+                            Console.WriteLine($"{item.BlogId}) {item.Name}");
+                        }
+                        if (int.TryParse(Console.ReadLine(), out int BlogId))
+                        {
+                            if (db.Blogs.Any(b => b.BlogId == BlogId))
+                            {
+                                Post post = InputPost(db);
+                                if (post != null)
+                                {
+                                    post.BlogId = BlogId;
+                                    db.AddPost(post);
+                                    logger.Info("Post added - {title}", post.Title);
+                                }
+                            }
+                            else
+                            {
+                                logger.Error("There are no Blogs saved with that Id");
+                            }
+                        }
+                        else
+                        {
+                            logger.Error("Invalid Blog Id");
+                        }
+                    }
+                }
+        while (choice.ToLower() != "q");
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex.Message);
+            }
+            logger.Info("Program ended");
+        }
+    }
+}
